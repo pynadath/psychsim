ed9c5fa9e1b5f01c8110b1a57a5d100c64c9dbb3
David V. Pynadath <david.pynadath@gmail.com>
2016-09-03 22:34:42 -0700

    Added getLog module to extract Team of Rival logs from MySQL

---
+++ domains/teamofrivals/getLog.py
@@ -0,0 +1,54 @@
+from argparse import ArgumentParser
+import ConfigParser
+
+import csv
+
+import MySQLdb
+import MySQLdb.cursors
+
+def readLog(gameID,cursor):
+    for player in range(4):
+        # Read in-game survey data from db
+        cursor.execute('SELECT * FROM sim_in_game_survey WHERE '+
+                       'sigs_game_id=%d ' % (gameID)+
+                       'AND sigs_player_number=%d' % (player+1))
+        results = cursor.fetchall()
+        fields = sorted(results[0].keys())
+        with open('%d_%d_survey_db.csv' % (gameID,player+1),'w') as csvfile:
+            writer = csv.DictWriter(csvfile,fields,extrasaction='ignore')
+            writer.writeheader()
+            for record in results:
+                writer.writerow(record)
+        # Read game log data from db
+        cursor.execute('SELECT spl_message FROM sim_project_log WHERE '+
+                       'spl_game_id=%d ' % (gameID)+
+                       'AND spl_player_number=%d' % (player+1))
+        results = cursor.fetchall()
+        fields = sorted(results[0].keys())
+        with open('%d_%d_logs.csv' % (gameID,player+1),'w') as csvfile:
+            writer = csv.DictWriter(csvfile,fields,extrasaction='ignore')
+            writer.writeheader()
+            for record in results:
+                writer.writerow(record)
+    
+if __name__ == '__main__':
+    config = ConfigParser.SafeConfigParser()
+    config.read('db.cfg')
+    if not config.has_section('mysql'):
+        config.add_section('mysql')
+        config.set('mysql','host','localhost')
+        config.set('mysql','user','')
+        config.set('mysql','passwd','')
+        with open('db.cfg','wb') as configfile:
+            config.write(configfile)
+    parser = ArgumentParser()
+    parser.add_argument('gameID',help='Game ID')
+    args = vars(parser.parse_args())
+        
+    db = MySQLdb.connect(host=config.get('mysql','host'),user=config.get('mysql','user'),
+                         passwd=config.get('mysql','passwd'),db='simproject',
+                         cursorclass=MySQLdb.cursors.DictCursor)
+    cursor = db.cursor()
+    readLog(int(args['gameID']),cursor)
+    cursor.close()
+    db.close()
