23d7c349060df27ae97a7a52d6d6f6a75e4ff66d
David V. Pynadath <david.pynadath@gmail.com>
2016-09-06 13:50:06 -0700

    Moved Key functions into separate module.

--- world.py
+++ world.py
@@ -4,10 +4,11 @@ import copy
 import StringIO
 from xml.dom.minidom import Document,Node,parseString
 
-from action import ActionSet,Action
-from pwl import *
-from probability import Distribution
-from agent import Agent
+from psychsim.action import ActionSet,Action
+from psychsim.keys import *
+from psychsim.probability import Distribution
+from psychsim.pwl import *
+from psychsim.agent import Agent
 
 class World:
     """
@@ -421,6 +422,8 @@ class World:
     """-----------------"""
 
     def addAgent(self,agent):
+        if isinstance(agent,str):
+            agent = Agent(agent)
         if self.has_agent(agent):
             raise NameError,'Agent %s already exists in this world' % (agent.name)
         else:
@@ -1765,95 +1768,6 @@ class World:
         f.write(self.__xml__().toprettyxml())
         f.close()
         return filename
-
-def stateKey(name,feature,future=False):
-    """
-    @param future: if C{True}, then this refers to the projected value of this feature (default is C{False})
-    @type future: bool
-    @return: a key representation of a given entity's state feature
-    @rtype: str
-    """
-    if isinstance(name,Agent):
-        return stateKey(name.name,feature,future)
-    elif future:
-        return stateKey(name,feature)+"'"
-    elif name is None:
-        return feature
-    else:
-        return '%s\'s %s' % (name,feature)
-
-def state2feature(key):
-    """
-    @return: the feature string from the given key
-    @rtype: str
-    """
-    index = key.find("'")
-    if index < 0:
-        return key
-    else:
-        return key[index+3:]
-    
-def makePresent(key):
-    """
-    @return: a reference to the given state features' current value
-    @rtype: str
-    """
-    if key[-1] == "'":
-        return key[:-1]
-    else:
-        raise ValueError,'%s is already a present key' % (key)
-def makeFuture(key):
-    """
-    @return: a reference to the given state features' projected future value
-    @rtype: str
-    """
-    if key[-1] == "'":
-        raise ValueError,'%s is already a future key' % (key)
-    else:
-        return key+"'"
-
-def isFuture(key):
-    return key[-1] == "'"
-
-def turnKey(name):
-    return stateKey(name,'_turn')
-
-def isTurnKey(key):
-    return key[-8:] == '\'s _turn'
-
-def turn2name(key):
-    return key[:-8]
-
-def actionKey(feature):    
-    return '__action__%s__' % (feature)
-
-def modelKey(name):
-    return stateKey(name,'_model')
-
-def isModelKey(key):
-    return key[-9:] == '\'s _model'
-
-def model2name(key):
-    return key[:-9]
-
-def binaryKey(subj,obj,relation):
-    return '%s %s -> %s' % (subj,relation,obj)
-
-def isBinaryKey(key):
-    return ' -> ' in key
-
-def key2relation(key):
-    sides = key.split(' -> ')
-    first = sides[0].split()
-    return {'subject': ' '.join(first[:-1]),
-            'object': sides[1],
-            'relation': first[-1]}
-
-def likesKey(subj,obj):
-    return binaryKey(subj,obj,'likes')
-
-def isLikesKey(key):
-    return ' likes -> ' in key
 
 def parseDomain(subnode):
     domain = str(subnode.getAttribute('domain'))
