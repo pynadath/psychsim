db133079fb7151393a62d5db76ac426ecfec782f
David V. Pynadath <david.pynadath@gmail.com>
2016-10-01 00:48:55 -0700

    Process WP surveys and provide some more HRI data

--- domains/hri/wp.py
+++ domains/hri/wp.py
@@ -9,8 +9,16 @@ import robot
 import MySQLdb
 import MySQLdb.cursors
 
-userMap = {71: 11,
-           75: 74,
+surveyMap = {'WP018': 'x92899',
+             'WP025': 'emily.sexauer',
+             'WP068': 'benjamin.denn',
+             'WP070': 'XDFGDG-TDDFBA-SRBFAA-OWGRABEDGABAD-EWGRABEBARBWG-PBFDSQS-MDBDRQ',
+             'WP071': 'WP011',
+             'WP073': 'jake.dohrn@usma.edu',
+             'WP075': 'WP074',
+             'WP076': 'mitchell.brown',
+             'WP080': 'XDFGDG-TDDFBA-SRBFBW-OWGRABEDGABAD-EWGRABEBEWBRG-PBFESRS-MDBDRQ',
+             'WP102': 'WP102/TO105',
            }
 SEQUENCE = [
     {'explanation_mode': 'none','robot_ability': 'badSensor','robot_ack': 'no',
@@ -30,8 +38,24 @@ SEQUENCE = [
     {'explanation_mode': 'confidence','robot_ability': 'badSensor','robot_ack': 'yes',
      'robot_embodiment': 'robot'},
 ]
+
+def readSurvey(fname):
+    data = {}
+    with open(fname,'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data[row['Participant ID'].strip().replace(' ','').upper()] = row
+    return data
     
 if __name__ == '__main__':
+    # Read survey files
+    survey = []
+    remaining = []
+    survey.append(readSurvey('Survey_Background.csv'))
+    remaining.append(set(survey[-1].keys()))
+    for mission in range(8):
+        survey.append(readSurvey('Survey_Mission%d.csv' % (mission+1)))
+        remaining.append(set(survey[-1].keys()))
     # Open connection to database
     config = ConfigParser.SafeConfigParser()
     config.read('db.cfg')
@@ -62,8 +86,6 @@ if __name__ == '__main__':
                 continue
             # Read log file
             log = robot.readLogData('WP%03d' % (user),mission,'/home/david/HRI/WP')
-            if user in userMap:
-                user = userMap[user]
             # Make sure the first entry is creation (log entries are in reverse order)
             assert log[-1]['type'] == 'create'
             # Ignore debugging logs
@@ -92,9 +114,26 @@ if __name__ == '__main__':
     decisionCases = ['true+', 'false+', 'true-', 'false-']
     for user in valid:
         for mission in range(8):
-            print 'User %d, Mission %d' % (user,mission+1)
+#            print 'User %d, Mission %d' % (user,mission+1)
             # Process the data from this mission
-            datum = {'user': 'WP%03d' % (user),'mission': mission+1,'deaths': 0}
+            datum = {'gameID': 'WP%03d' % (user),
+                     'mission': mission+1,
+                     'deaths': 0,
+            }
+            # Check for surveys
+            try:
+                datum['surveyID'] = surveyMap[datum['gameID']].upper()
+            except KeyError:
+                datum['surveyID'] = datum['gameID']
+            if mission == 0:
+                if not datum['surveyID'] in survey[0]:
+                    print >> sys.stderr,'User %d did not fill out background survey' % (user)
+                else:
+                    remaining[0].remove(datum['surveyID'])
+            if not datum['surveyID'] in survey[mission+1]:
+                print >> sys.stderr,'User %d did not fill out post-survey for mission %d' % (user,mission+1)
+            else:
+                remaining[mission+1].remove(datum['surveyID'])
             for case in decisionCases:
                 datum[case] = 0
             for case in decisionCases:
@@ -148,13 +187,18 @@ if __name__ == '__main__':
             if decisions != 15:
                 print '\n'.join(['%s\n%s' % (e['type'],str(e)) for e in log])
                 raise ValueError,'\tOnly %d/15 decisions\n%s' % (decisions,datum)
+            datum['follow'] = sum([datum['follow_%s' % (case)] for case in decisionCases])
+            datum['ignore'] = sum([datum['ignore_%s' % (case)] for case in decisionCases])
     # Write data to file
     with open('wpdata.csv', 'w') as csvfile:
-        fieldnames = ['user','mission','explanation','acknowledgment','embodiment','time','deaths']
+        fieldnames = ['gameID','surveyID','mission','explanation','acknowledgment','embodiment','time','deaths']
         fieldnames += decisionCases
+        fieldnames += ['follow','ignore']
         fieldnames += ['follow_%s' % (case) for case in decisionCases]
         fieldnames += ['ignore_%s' % (case) for case in decisionCases]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for datum in data:
             writer.writerow(datum)
+    for ids in remaining:
+        print >> sys.stderr,ids
