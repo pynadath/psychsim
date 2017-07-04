1345b84a5622389a0d9482ed440786fbecb60c22
David V. Pynadath <david@pynadath.net>
2017-07-04 03:12:38 -0700

    Divided pwl module into pwl package

---
+++ pwl/__init__.py
@@ -0,0 +1,10 @@
+"""
+Class and function definitions for PieceWise Linear (PWL) representations
+"""
+
+CONSTANT = ''
+
+from vector import *
+from matrix import *
+from plane import *
+from tree import *
