21192ce2058c878f495832d43a5d7a0d6b533570
David V. Pynadath <david@pynadath.net>
2018-08-11 15:51:00 -0700

    Generate TOC for LaTex version

--- tools/world2tex.py
+++ tools/world2tex.py
@@ -10,38 +10,47 @@ from psychsim.world import World
 from psychsim.graph import DependencyGraph
 
 def addGraph(doc,key,fname):
-    with doc.create(Figure()) as gtGraph:
+    with doc.create(Figure(position='ht')) as gtGraph:
         gtGraph.add_image('%s' % (os.path.join('images','%s.png' % (fname))))
         gtGraph.add_caption('Ground Truth subgraph for %s' % (key))
     
-def addTree(doc,tree,indent=0):
-    with doc.create(FlushLeft()):
-        if indent:
-            doc.append(HorizontalSpace('%dem' % (indent)))
-        if tree.isLeaf():
-            matrix = tree.children[None]
-            assert len(matrix) == 1
-            key,row = matrix.items()[0]
-            doc.append(key)
-            doc.appendMath('\leftarrow')#'%s $\leftarrow$ %s' % (key,row))
-        elif tree.isProbabilistic():
-            doc.append('DISTRIBUTION')
-        else:
-            doc.append('BRANCH')
+def addTree(doc,tree,indent=0,prefix=None):
+    if indent:
+        doc.append(HorizontalSpace('%dem' % (indent)))
+    if prefix:
+        doc.append('%s: ' % (prefix))
+    if tree.isLeaf():
+        matrix = tree.children[None]
+        assert len(matrix) == 1
+        key,row = matrix.items()[0]
+        doc.append(makePresent(key))
+    elif tree.isProbabilistic():
+        for value in tree.children.domain():
+            doc.append(LineBreak())
+            addTree(doc,value,indent+2,'%d' % (tree.children[value]*100))
+    else:
+        doc.append('IF %s' % (str(tree.branch)))
+        for value,child in tree.children.items():
+            doc.append(LineBreak())
+            addTree(doc,child,indent+2,value)
 #        doc.append(escape_latex(line.strip()))
-        doc.append(LineBreak())
+    doc.append(LineBreak())
     
 if __name__ == '__main__':
     world = World(sys.argv[1])
+
+    graph = DependencyGraph(world)
+    graph.computeGraph()
 
     doc = Document(geometry_options={'tmargin': '1in', 
                                      'lmargin': '1in',
                                      'textwidth': '6.5in',
                                      'textheight': '9in'})
 
-    graph = DependencyGraph(world)
-    graph.computeGraph()
-
+    doc.preamble.append(Command('usepackage','hyperref'))
+    doc.preamble.append(Command('hypersetup','colorlinks'))
+    doc.append(NoEscape(r'\tableofcontents'))
+    doc.append(NoEscape(r'\clearpage'))    
     with doc.create(Section('State')):
         for name,variable in sorted(world.variables.items()):
             if isStateKey(name):
@@ -49,11 +58,15 @@ if __name__ == '__main__':
                     with doc.create(Subsection(name)):
                         addGraph(doc,name,escapeKey(name))
                         for action,tree in sorted(world.dynamics[name].items()):
+                            if state2agent(name) == WORLD:
+                                name = state2feature(name)
                             if action is True:
-                                action = 'Default'
-                            assert action != None
-                            with doc.create(Subsubsection(str(action))):
-                                addTree(doc,tree)
+                                heading = 'Default Change in %s' % (name)
+                            else:
+                                heading = 'Effect of %s on %s' % (str(action),name)
+                            with doc.create(Subsubsection(heading)):
+                                with doc.create(FlushLeft()):
+                                    addTree(doc,tree)
     with doc.create(Section('Actions')):
         for name,agent in world.agents.items():
             for action in sorted(agent.actions):
@@ -64,11 +77,14 @@ if __name__ == '__main__':
                 with doc.create(Subsection(label)):
                     addGraph(doc,action,str(action))
                     for key,table in sorted(world.dynamics.items()):
+                        if state2agent(key) == WORLD:
+                            key = state2feature(key)
                         for other,tree in table.items():
                             if other == action:
-                                with doc.create(Subsubsection(key)):
-                                    addTree(doc,tree)
+                                with doc.create(Subsubsection('Effect on %s of %s' % (key,label))):
+                                    with doc.create(FlushLeft()):
+                                        addTree(doc,tree)
                                 break
                             
 
-    doc.generate_pdf('groundTruth')
+    doc.generate_pdf('groundTruth',clean_tex=False)
