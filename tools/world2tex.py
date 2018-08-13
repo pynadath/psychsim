b0206e3bd3069c53bb1858e1981caf9e97f4d42c
David V. Pynadath <david@pynadath.net>
2018-08-12 23:52:28 -0700

    Added background section to LaTeX generation

--- tools/world2tex.py
+++ tools/world2tex.py
@@ -1,3 +1,4 @@
+import inspect
 import os
 import sys
 
@@ -32,62 +33,68 @@ def addTree(doc,tree,world,indent=0,prefix=None):
         doc.append('%s ' % (prefix))
     if tree.isLeaf():
         matrix = tree.children[None]
-        assert len(matrix) == 1
-        key,row = matrix.items()[0]
-        variable = world.variables[makePresent(key)]
-        doc.append(key2tex(key))
-        doc.append(Math(data=Command('leftarrow'),inline=True))
-        if variable['domain'] is bool:
-            assert len(row) == 1
-            if CONSTANT in row:
-                if row[CONSTANT] > 0.5:
-                    doc.append(bold('true'))
-                else:
-                    doc.append(bold('false'))
+        if isinstance(matrix,bool):
+            if matrix:
+                doc.append(bold('true'))
             else:
-                subkey = row.keys()[0]
-                assert subkey == makePresent(key) in row
-                if row[subkey] < 0.:
-                    doc.append(Math(data=Command('lnot'),inline=True))
-                doc.append(key2tex(subkey))
-        elif variable['domain'] is set or variable['domain'] is list:
-            assert len(row) == 1
-            subkey = row.keys()[0]
-            if subkey == CONSTANT:
-                doc.append(bold(world.float2value(makePresent(key),row[CONSTANT])))
-            else:
-                assert row[subkey] == 1.
-                doc.append(key2tex(subkey))
+                doc.append(bold('false'))
         else:
-            elements = {key: value for key,value in row.items()}
-            first = True
-            for key in sorted(elements.keys()):
-                value = elements[key]
-                if key != CONSTANT:
-                    if first:
-                        first = True
+            assert len(matrix) == 1
+            key,row = matrix.items()[0]
+            variable = world.variables[makePresent(key)]
+            doc.append(key2tex(key))
+            doc.append(Math(data=Command('leftarrow'),inline=True))
+            if variable['domain'] is bool:
+                assert len(row) == 1
+                if CONSTANT in row:
+                    if row[CONSTANT] > 0.5:
+                        doc.append(bold('true'))
                     else:
-                        doc.append('+')
-                    if value != 1.:
-                        doc.append('%d%%' % (100.*value))
-                        doc.append(Math(data=Command('cdot'),inline=True))
-                    doc.append(key2tex(key))
-            if CONSTANT in elements:
-                if elements[CONSTANT] < 0.:
-                    if variable['domain'] is int:
-                        doc.append(Math(data='-%d' % (abs(elements[CONSTANT])),inline=True))
-                    else:
-                        doc.append(Math(data='-%4.2f' % (abs(elements[CONSTANT])),inline=True))
-                elif len(elements) == 1:
-                    if variable['domain'] is int:
-                        doc.append('%d' % (elements[CONSTANT]))
-                    else:
-                        doc.append('%4.2f' % (elements[CONSTANT]))
-                elif elements[CONSTANT] > 0.:
-                    if variable['domain'] is int:
-                        doc.append('+%d' % (elements[CONSTANT]))
-                    else:
-                        doc.append('+%4.2f' % (elements[CONSTANT]))
+                        doc.append(bold('false'))
+                else:
+                    subkey = row.keys()[0]
+                    assert subkey == makePresent(key) in row
+                    if row[subkey] < 0.:
+                        doc.append(Math(data=Command('lnot'),inline=True))
+                    doc.append(key2tex(subkey))
+            elif variable['domain'] is set or variable['domain'] is list:
+                assert len(row) == 1
+                subkey = row.keys()[0]
+                if subkey == CONSTANT:
+                    doc.append(bold(world.float2value(makePresent(key),row[CONSTANT])))
+                else:
+                    assert row[subkey] == 1.
+                    doc.append(key2tex(subkey))
+            else:
+                elements = {key: value for key,value in row.items()}
+                first = True
+                for key in sorted(elements.keys()):
+                    value = elements[key]
+                    if key != CONSTANT:
+                        if first:
+                            first = True
+                        else:
+                            doc.append('+')
+                        if value != 1.:
+                            doc.append('%d%%' % (100.*value))
+                            doc.append(Math(data=Command('cdot'),inline=True))
+                        doc.append(key2tex(key))
+                if CONSTANT in elements:
+                    if elements[CONSTANT] < 0.:
+                        if variable['domain'] is int:
+                            doc.append(Math(data='-%d' % (abs(elements[CONSTANT])),inline=True))
+                        else:
+                            doc.append(Math(data='-%4.2f' % (abs(elements[CONSTANT])),inline=True))
+                    elif len(elements) == 1:
+                        if variable['domain'] is int:
+                            doc.append('%d' % (elements[CONSTANT]))
+                        else:
+                            doc.append('%4.2f' % (elements[CONSTANT]))
+                    elif elements[CONSTANT] > 0.:
+                        if variable['domain'] is int:
+                            doc.append('+%d' % (elements[CONSTANT]))
+                        else:
+                            doc.append('+%4.2f' % (elements[CONSTANT]))
     elif tree.isProbabilistic():
         if len(tree.children) == 1:
             addTree(doc,tree.children.domain()[0],world,0)
@@ -117,7 +124,13 @@ def addTree(doc,tree,world,indent=0,prefix=None):
                     if len(vector) == 1:
                         assert abs(vector[key]-1.) < 1e-6,tree.branch
                         doc.append(Math(data='=',inline=True))
-                        doc.append(bold(world.float2value(makePresent(key),threshold)))
+                        if isinstance(threshold,list):
+                            for value in sorted(threshold):
+                                if value != sorted(threshold)[0]:
+                                    doc.append(' or ')
+                                doc.append(bold(world.float2value(makePresent(key),value)))
+                        else:
+                            doc.append(bold(world.float2value(makePresent(key),threshold)))
                         done = True
                     else:
                         assert len(vector) == 2
@@ -176,6 +189,20 @@ def addState(doc,world):
                     else:
                         label = name
                     with doc.create(Subsection(label)):
+                        if variable['domain'] is bool:
+                            doc.append('Boolean')
+                        elif variable['domain'] is int:
+                            doc.append('Integer')
+                        elif variable['domain'] is float:
+                            doc.append('Real')
+                        else:
+                            doc.append('String: ')
+                            for value in sorted(variable['elements']):
+                                if value == sorted(variable['elements'])[0]:
+                                    doc.append(bold(value))
+                                else:
+                                    doc.append(', ')
+                                    doc.append(bold(value))
                         addGraph(doc,name,escapeKey(name))
                         for action,tree in sorted(world.dynamics[name].items()):
                             if action is True:
@@ -194,6 +221,20 @@ def addRelations(doc,world):
                     relation = key2relation(name)
                     label = '%s %s %s' % (relation['subject'],relation['relation'],relation['object'])
                     with doc.create(Subsection(label)):
+                        if variable['domain'] is bool:
+                            doc.append('Boolean')
+                        elif variable['domain'] is int:
+                            doc.append('Integer')
+                        elif variable['domain'] is float:
+                            doc.append('Real')
+                        else:
+                            doc.append('String: ')
+                            for value in sorted(variable['elements']):
+                                if value == sorted(variable['elements'])[0]:
+                                    doc.append(bold(value))
+                                else:
+                                    doc.append(', ')
+                                    doc.append(bold(value))
                         addGraph(doc,name,escapeKey(name))
                         for action,tree in sorted(world.dynamics[name].items()):
                             if action is True:
@@ -214,6 +255,10 @@ def addActions(doc,world):
                     label = '%s %s' % (action['subject'],action['verb'])
                 with doc.create(Subsection(label)):
                     addGraph(doc,action,str(action))
+                    if action in agent.legal:
+                        with doc.create(Subsubsection('Applicability Conditions')):
+                            with doc.create(FlushLeft()):
+                                addTree(doc,agent.legal[action],world)
                     for key,table in sorted(world.dynamics.items()):
                         if state2agent(key) == WORLD:
                             key = state2feature(key)
@@ -228,6 +273,25 @@ def addActions(doc,world):
                                         addTree(doc,tree,world)
                                 break
 
+def background(doc):
+    filename = inspect.getframeinfo(inspect.currentframe()).filename
+    path = os.path.dirname(os.path.abspath(filename))
+    with doc.create(Section('Background')):
+        doc.append('We use influence diagrams as the underlying graph structure for our ground truth. Here is a simple influence diagram for a simulation of two actors, showing the three types of nodes and some possible links (always directed) among them:')
+        with doc.create(Figure(position='ht')) as gtGraph:
+            gtGraph.add_image(os.path.join(path,'simple.png'))
+            gtGraph.add_caption('Simple influence diagram')
+        with doc.create(Itemize()) as itemize:
+            itemize.add_item('Rectangular nodes are possible actions for a particular agent (``Actor 1\'\', indicated by color) representing a potential behavior. They are labeled with a verb (``action\'\') and an optional object of the verb (``Actor2\'\'). An action node has a binary value, indicating whether or not the action was chosen.')
+            itemize.add_item('Oval nodes are state variables. Their value is potentially a probability distribution over a domain of possible values. All true state variables will be certain (i.e., 100\% probability for a single value), but agents\' perceptions of the true state will often be uncertain.')
+            itemize.add_item('Hexagonal nodes are utility or reward nodes. They represent an expected value computation by the agent (``Actor1\'\'). The node\'s value is a table with each row corresponding to a possible action choice and its expected utility.')
+            itemize.add_item('Links from action nodes to state nodes specify an effect that the action has on the value of the state.')
+            itemize.add_item('Links from one state node to another specify an influence that the value of the first state node has on the effect of at least one action on the second state node.')
+            itemize.add_item('Links from a state node to an agent\'s utility node specify that the state node is an input to the expected value calculation performed by that agent. There is a real-valued weight from $(0,1\]$ on each link specifying the priority of that variable\'s influence on that agent\'s reward calculation (higher values mean higher priority).')
+            itemize.add_item('Links from utility nodes to action nodes indicate that the expected value calculation then determines whether or not that action is chosen. In the simulations described here, we use a strict maximization, so that the action choice is deterministic (i.e., the action with the highest expected value is performed, with ties broken by a pre-determined fixed order).')
+            itemize.add_item('Therefore, in the above simple ground truth, whether or not ``Actor1\'\' chooses to do ``action\'\' to ``Actor2\'\' influences the subsequent value of the variable ``state\'\' (link from rectangle to oval). The subsequent value of ``state\'\' also depends on its prior value (link from oval to itself). ``Actor1\'\'\'s expected value of doing ``action\'\' to ``Actor2\'\' is a function of the value of ``state\'\' (link from oval to hexagon), and this expected value influences whether or not ``Actor1\'\' chooses to do so (link from hexagon to rectangle).')
+        doc.append('Any real values (e.g., initial values of variables, conditional probability table values, reward weights) will be drawn from either a set {0, 0.5, 1} or {0, 0.2, 0.4, 0.6, 0.8, 1}, depending on the appropriate granularity needed.')
+    
 def createDoc():
     doc = Document(geometry_options={'tmargin': '1in', 
                                      'lmargin': '1in',
@@ -238,6 +302,7 @@ def createDoc():
     doc.preamble.append(Command('hypersetup','colorlinks'))
     doc.append(NoEscape(r'\tableofcontents'))
     doc.append(NoEscape(r'\clearpage'))
+    background(doc)
     return doc
     
 if __name__ == '__main__':
