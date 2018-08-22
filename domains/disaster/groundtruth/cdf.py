96f410826168e2141ea8b779573fbd7c07b46689
David V. Pynadath <david@pynadath.net>
2018-08-22 03:34:36 -0700

    Separated definition from instance CDF files

--- domains/disaster/groundtruth/cdf.py
+++ domains/disaster/groundtruth/cdf.py
@@ -18,42 +18,50 @@ def shorten(key):
         return '%s %s' % (state2agent(key),state2feature(key))
     else:
         return key
-    
-def toCDF(world,unobservable=set()):
-    with open(os.path.join('SimulationDefinition','VariableDefTable'),'w') as csvfile:
-        fields = ['Name','LongName','Values','Observable','VarType','DataType','Notes']
+
+def var2def(name,world,base=None):
+    if base is None:
+        base = name
+    variable = world.variables[base]
+    record = {'Name': shorten(name),
+              'LongName': name,
+              'Notes': variable['description'],
+              'DataType': dataTypes[variable['domain']],
+    }
+    if variable['domain'] is bool:
+        record['Values'] = 'True,False'
+    elif variable['domain'] is list:
+        record['Values'] = ','.join(variable['elements'])
+    elif variable['domain'] is ActionSet:
+        record['Values'] = ','.join(map(str,variable['elements']))
+    elif variable['domain'] is float:
+        record['Values'] = '[%4.2f-%4.2f]' % (variable['lo'],variable['hi'])
+    elif variable['domain'] is int:
+        record['Values'] = '[%d-%d]' % (variable['lo'],variable['hi'])
+    else:
+        raise TypeError('Unable to write values for variables of type %s' \
+                        % (variable['domain'].__name__))
+    if name in world.dynamics and not isRewardKey(name):
+        record['VarType'] = 'dynamic'
+    else:
+        record['VarType'] = 'fixed'
+    return record
+
+def writeDefinition(world,dirName):
+    with open(os.path.join(dirName,'VariableDefTable'),'w') as csvfile:
+        fields = ['Name','LongName','Values','VarType','DataType','Notes']
         writer = csv.DictWriter(csvfile,fields,delimiter='\t',
                                 extrasaction='ignore')
         writer.writeheader()
-        for name,variable in sorted(world.variables.items()):
+        for name in sorted(world.variables.keys()):
             if isModelKey(name) or isTurnKey(name) or isBinaryKey(name):
                 continue
-            record = {'Name': shorten(name),
-                      'LongName': name,
-                      'Notes': variable['description'],
-                      'Observable': {True: 1, False: 0}[not name in unobservable],
-                      'DataType': dataTypes[variable['domain']],
-            }
-            if variable['domain'] is bool:
-                record['Values'] = 'True,False'
-            elif variable['domain'] is list:
-                record['Values'] = ','.join(variable['elements'])
-            elif variable['domain'] is ActionSet:
-                record['Values'] = ','.join(map(str,variable['elements']))
-            elif variable['domain'] is float:
-                record['Values'] = '[%4.2f-%4.2f]' % (variable['lo'],variable['hi'])
-            elif variable['domain'] is int:
-                record['Values'] = '[%d-%d]' % (variable['lo'],variable['hi'])
-            else:
-                raise TypeError('Unable to write values for variables of type %s' \
-                                % (variable['domain'].__name__))
-            if name in world.dynamics and not isRewardKey(name):
-                record['VarType'] = 'dynamic'
-            else:
-                record['VarType'] = 'fixed'
-            writer.writerow(record)
+            # State variables
+            writer.writerow(var2def(name,world))
         for name,agent in world.agents.items():
-            tree = agent.models['%s0' % (agent.name)]['R']
+            # Reward variables
+            model = '%s0' % (agent.name)
+            tree = agent.models[model]['R']
             if tree.isLeaf():
                 vector = tree.children[None][makeFuture(rewardKey(name))]
                 if len(vector) == 1 and abs(vector[CONSTANT]) < 1e-8:
@@ -61,42 +69,24 @@ def toCDF(world,unobservable=set()):
             record = {'Name': 'Reward%s' % (name),
                       'LongName': '%s\'s Reward' % (name),
                       'Values': '[-1.0-1.0]',
-                      'Observable': 0,
                       'Type': 'dynamic',
                       }
             writer.writerow(record)
+            # Beliefs
+            belief = agent.getBelief(world.state,model)
+            for name in belief.keys():
+                pass
     with open(os.path.join('SimulationDefinition','RelationshipDefTable'),'w') as csvfile:
         fields = ['Name','LongName','Values','Observable','VarType','DataType','Notes']
         writer = csv.DictWriter(csvfile,fields,delimiter='\t',
                                 extrasaction='ignore')
         writer.writeheader()
-        for name,variable in sorted(world.variables.items()):
+        for name in sorted(world.variables.keys()):
             if isBinaryKey(name):
-                record = {'Name': shorten(name),
-                          'LongName': name,
-                          'Notes': variable['description'],
-                          'Observable': {True: 1, False: 0}[not name in unobservable],
-                          'DataType': dataTypes[variable['domain']],
-                }
-                if variable['domain'] is bool:
-                    record['Values'] = 'True,False'
-                elif variable['domain'] is list:
-                    record['Values'] = ','.join(variable['elements'])
-                elif variable['domain'] is ActionSet:
-                    record['Values'] = ','.join(map(str,variable['elements']))
-                elif variable['domain'] is float:
-                    record['Values'] = '[%4.2f-%4.2f]' % (variable['lo'],variable['hi'])
-                elif variable['domain'] is int:
-                    record['Values'] = '[%d-%d]' % (variable['lo'],variable['hi'])
-                else:
-                    raise TypeError('Unable to write values for variables of type %s' \
-                                    % (variable['domain'].__name__))
-                if name in world.dynamics:
-                    record['VarType'] = 'dynamic'
-                else:
-                    record['VarType'] = 'fixed'
-                writer.writerow(record)
-    with open(os.path.join('SimulationDefinition','InstanceVariableTable'),'w') as csvfile:
+                writer.writerow(var2def(name,world))
+                
+def toCDF(world,dirName,unobservable=set()):
+    with open(os.path.join(dirName,'InstanceVariableTable'),'w') as csvfile:
         fields = ['Name','Timestep','Value']
         writer = csv.DictWriter(csvfile,fields,delimiter='\t',extrasaction='ignore')
         writer.writeheader()
