04ec9c4982dd24e6bd2421f3fa74b6e9a67de10d
David V. Pynadath <david@pynadath.net>
2018-09-10 01:39:41 -0700

    Added script for running flexibility tests

---
+++ domains/groundtruth/flexibility.fish
@@ -0,0 +1,12 @@
+set -x PYTHONHASHSEED=1
+gt -n 3 -i 0 -r 10
+gt -n 3 -i 1 -r 10
+gt -n 3 -i 2 -r 10
+gt -n 3 -i 3 -r 10
+gt -n 3 -i 4 -r 10
+gt -n 3 -i 5 -r 10
+gt -n 3 -i 6 -r 10
+gt -n 3 -i 7 -r 10
+gt -n 3 -i 8 -r 10
+gt -n 3 -i 9 -r 10
+
