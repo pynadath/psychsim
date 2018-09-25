e5cdacb9b10e4ab14e92d9e73bed25ec3c51ea6a
David V. Pynadath <david@pynadath.net>
2018-09-25 15:53:51 -0700

    Catch exception in psychsim.world if Qt is not installed

--- world.py
+++ world.py
@@ -11,7 +11,11 @@ import psychsim.probability
 from psychsim.pwl import *
 from psychsim.agent import Agent
 import psychsim.graph
-from psychsim.ui.diagram import Diagram
+
+try:
+    from psychsim.ui.diagram import Diagram
+except:
+    pass
 
 class World(object):
     """
