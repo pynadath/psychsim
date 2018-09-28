d2183314d97240215c4531b7d7d7e708f57b7643
David V. Pynadath <david@pynadath.net>
2018-09-28 01:28:21 -0700

    Added census query module

---
+++ domains/groundtruth/accessibility/census.py
@@ -0,0 +1,60 @@
+from argparse import ArgumentParser
+import csv
+import os
+
+from psychsim.world import World
+from psychsim.domains.groundtruth.region import Region
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('instance',type=int,help='Instance to query')
+    parser.add_argument('-o','--output',default='RequestCensusTable.tsv',help='Output filename')
+    parser.add_argument('--region',type=int,help='Specific region for query')
+    parser.add_argument('--days',help='Days of query')
+    parser.add_argument('fields',nargs='+',help='Fields to include in report')
+    args = vars(parser.parse_args())
+
+    root = os.path.join(os.path.dirname(__file__),'..')
+    inFile = os.path.join(root,'Instances','Instance%d' % (args['instance']),
+                          'Runs','run-0','RunDataTable.tsv')
+    population = {'__population__': set()}
+    if args['region']:
+        subset = Region.nameString % (args['region'])
+    else:
+        subset = 'All'
+    with open(inFile,'r') as csvfile:
+        reader = csv.DictReader(csvfile,delimiter='\t')
+        for row in reader:
+            variable = row['VariableName'].split()
+            if variable[0] == 'Actor':
+                if variable[1] in args['fields']:
+                    if not row['EntityIdx'] in population:
+                        population[row['EntityIdx']] = {}
+                    population[row['EntityIdx']][variable[1]] = row['Value']
+                if variable[1] == 'region':
+                    if args['region']:
+                        if row['Value'] == subset:
+                            population['__population__'].add(row['EntityIdx'])
+                    else:
+                        population['__population__'].add(row['EntityIdx'])
+
+    outFile = os.path.join(root,'Instances','Instance%d' % (args['instance']),
+                           'Runs','run-0',args['output'])
+    fields = ['EntityIdx','Variable','Value']
+    with open(outFile,'w') as csvfile:
+        writer = csv.DictWriter(csvfile,fields,delimiter='\t',extrasaction='ignore')
+        writer.writeheader()
+        if 'population' in args['fields']:
+            record = {'EntityIdx': subset,
+                      'Variable': 'Population',
+                      'Value': len(population['__population__'])}
+            writer.writerow(record)
+        for actor in population['__population__']:
+            for field in args['fields']:
+                if field != 'population':
+                    record = {'EntityIdx': actor,
+                              'Variable': field,
+                              'Value': population[actor][field]}
+                    writer.writerow(record)
+                    
+                
