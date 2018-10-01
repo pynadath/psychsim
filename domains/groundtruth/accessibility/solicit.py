dedf0714ca981259c16bcf39485b96f08e443493
David V. Pynadath <david@pynadath.net>
2018-09-30 23:57:44 -0700

    Generalized information solicitation module

--- domains/groundtruth/accessibility/solicit.py
+++ domains/groundtruth/accessibility/solicit.py
@@ -1,5 +1,7 @@
+from argparse import ArgumentParser
 import csv
 import os
+import pickle
 import random
 import sys
 
@@ -7,36 +9,46 @@ from psychsim.pwl.keys import *
 
 from psychsim.world import World
 
-instance = int(sys.argv[1])
-inFile = os.path.join(os.path.dirname(__file__),'Instances','Instance%d' % (instance),
-                      'Runs','run-0','scenario.psy')
-
 
 mapping = {'childrenHealth': 'children',
            'resources': 'finances'}
 
-samples = []
-world = World(inFile)
-actors = [actor for actor in world.agents.values() if actor.name[:5] == 'Actor' and \
-          actor.getState('alive').first()]
-outFile = os.path.join(os.path.dirname(__file__),'Instances','Instance100001',
-                       'Runs','run-0','AccessibilityDemo07Table')
-fields = ['Actor','Factor1','Factor2','Factor3']
-with open(outFile,'w') as csvfile:
-    writer = csv.DictWriter(csvfile,fields,delimiter='\t',extrasaction='ignore')
-    writer.writeheader()
-    while len(samples) < 10:
-        actor = random.choice(actors)
-        actors.remove(actor)
-        samples.append({'Actor': actor.name})
-        model = world.getModel(actor.name).first()
-        R = actor.getAttribute('R',model)
-        assert R.isLeaf()
-        R = R.children[None][makeFuture(rewardKey(actor.name))]
-        weights = [(R[key],key) for key in R.keys() if key != CONSTANT]
-        weights.sort()
-        weights.reverse()
-        for index in range(min(len(weights),3)):
-            feature = state2feature(weights[index][1])
-            samples[-1]['Factor%d' % (index+1)] = 'my %s' % (mapping.get(feature,feature))
-        writer.writerow(samples[-1])
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument('instance',type=int,help='Instance to query')
+    parser.add_argument('-o','--output',default='RequestInfoSolicit.tsv',help='Output filename')
+    parser.add_argument('-r','--run',type=int,default=0,help='Run to query')
+    parser.add_argument('-s','--samples',type=int,default=10,help='Number of people to survey')
+    args = vars(parser.parse_args())
+
+    root = os.path.join(os.path.dirname(__file__),'..')
+    inFile = os.path.join(root,'Instances','Instance%d' % (args['instance']),
+                          'Runs','run-%d' % (args['run']),'scenario.pkl')
+    with open(inFile,'rb') as f:
+        world = pickle.load(f)
+
+
+    samples = []
+    actors = [actor for actor in world.agents.values() if actor.name[:5] == 'Actor' and \
+              actor.getState('alive').first()]
+    outFile = os.path.join(root,'Instances','Instance%d' % (args['instance']),
+                           'Runs','run-%d' % (args['run']),args['output'])
+    fields = ['ParticipantId','Factor1','Factor2','Factor3']
+    with open(outFile,'w') as csvfile:
+        writer = csv.DictWriter(csvfile,fields,delimiter='\t',extrasaction='ignore')
+        writer.writeheader()
+        while len(samples) < args['samples'] and actors:
+            actor = random.choice(actors)
+            actors.remove(actor)
+            samples.append({'ParticipantId': len(samples)+1})
+            model = world.getModel(actor.name).first()
+            R = actor.getAttribute('R',model)
+            assert R.isLeaf()
+            R = R.children[None][makeFuture(rewardKey(actor.name))]
+            weights = [(R[key],key) for key in R.keys() if key != CONSTANT]
+            weights.sort()
+            weights.reverse()
+            for index in range(min(len(weights),3)):
+                feature = state2feature(weights[index][1])
+                samples[-1]['Factor%d' % (index+1)] = 'my %s' % (mapping.get(feature,feature))
+            writer.writerow(samples[-1])
