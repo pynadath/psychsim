8703740dee50fda037e1ddcc6ecada6bd6ff951d
David V. Pynadath <david@pynadath.net>
2018-10-01 15:46:35 -0700

    Updated flexibility script

--- domains/groundtruth/flexibility.fish
+++ domains/groundtruth/flexibility.fish
@@ -1,12 +1,14 @@
+#!/usr/bin/fish
+cd (dirname (status -f))
 set -x PYTHONHASHSEED=1
-gt -n 3 -i 0 -r 10
-gt -n 3 -i 1 -r 10
-gt -n 3 -i 2 -r 10
-gt -n 3 -i 3 -r 10
-gt -n 3 -i 4 -r 10
-gt -n 3 -i 5 -r 10
-gt -n 3 -i 6 -r 10
-gt -n 3 -i 7 -r 10
-gt -n 3 -i 8 -r 10
-gt -n 3 -i 9 -r 10
+python3 . -n 3 -i 0 -r 10
+python3 . -n 3 -i 1 -r 10
+python3 . -n 3 -i 2 -r 10
+python3 . -n 3 -i 3 -r 10
+python3 . -n 3 -i 4 -r 10
+python3 . -n 3 -i 5 -r 10
+python3 . -n 3 -i 6 -r 10
+python3 . -n 3 -i 7 -r 10
+python3 . -n 3 -i 8 -r 10
+python3 . -n 3 -i 9 -r 10
 
