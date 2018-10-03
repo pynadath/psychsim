60c625f88db30eee4932d8941b3a83d0fee37f78
David V. Pynadath <david@pynadath.net>
2018-10-03 02:13:51 -0700

    Pulled out hurricane reading module

--- domains/groundtruth/accessibility/environmental.py
+++ domains/groundtruth/accessibility/environmental.py
@@ -2,6 +2,21 @@ from argparse import ArgumentParser
 import csv
 import os
 
+def readHurricanes(instance,run,hurricane=None):
+    inFile = os.path.join(os.path.dirname(__file__),'..','Instances','Instance%d' % (instance),
+                          'Runs','run-%d' % (run),'HurricaneTable.tsv')
+    sequence = []
+    with open(inFile,'r') as csvfile:
+        reader = csv.DictReader(csvfile,delimiter='\t')
+        for row in reader:
+            current = int(row['Name'])
+            if len(sequence) < current:
+                sequence.append([])
+            if hurricane and hurricane != int(row['Name']):
+                continue
+            sequence[-1].append(row)
+    return sequence
+
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('instance',type=int,help='Instance to query')
@@ -11,22 +26,15 @@ if __name__ == '__main__':
     parser.add_argument('fields',nargs='+',help='Fields to include in report')
     args = vars(parser.parse_args())
     root = os.path.join(os.path.dirname(__file__),'..')
-    inFile = os.path.join(root,'Instances','Instance%d' % (args['instance']),
-                          'Runs','run-%d' % (args['run']),'HurricaneTable.tsv')
-    sequence = []
-    with open(inFile,'r') as csvfile:
-        reader = csv.DictReader(csvfile,delimiter='\t')
-        for row in reader:
-            if args['hurricane'] and args['hurricane'] != int(row['Name']):
-                continue
-            sequence.append(row)
+    sequence = readHurricanes(args['instance'],args['run'],args['hurricane'])
     outFile = os.path.join(root,'Instances','Instance%d' % (args['instance']),
                            'Runs','run-%d' % (args['run']),args['output'])
     fields = ['Timestep','Name']+args['fields']
     with open(outFile,'w') as csvfile:
         writer = csv.DictWriter(csvfile,fields,delimiter='\t',extrasaction='ignore')
         writer.writeheader()
-        for row in sequence:
-            writer.writerow(row)
+        for hurricane in sequence:
+            for row in hurricane:
+                writer.writerow(row)
             
     
