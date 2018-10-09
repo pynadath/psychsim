a4cec126d75b0362126ce1c17eaaea36d65dd4a8
David V. Pynadath <david@pynadath.net>
2018-10-09 00:45:43 -0700

    Added explicit rewards back to System

--- domains/groundtruth/system.py
+++ domains/groundtruth/system.py
@@ -28,16 +28,16 @@ class System(Agent):
         populated = set()
         for actor in population:
             populated.add(world.getState(actor,'region').first())
-#            self.setReward(maximizeFeature(stateKey(actor,'health'),self.name),1.)
-#            self.setReward(minimizeFeature(stateKey(actor,'grievance'),self.name),1.)
+            self.setReward(maximizeFeature(stateKey(actor,'health'),self.name),
+                           likert[5][config.getint('System','reward_health')-1])
+            self.setReward(minimizeFeature(stateKey(actor,'grievance'),self.name),
+                           likert[5][config.getint('System','reward_grievance')-1])
         allocation = config.getint('System','system_allocation')
         for region in populated:
 #            tree = makeTree({'if': thresholdRow(resources,allocation),True: True,False: False})
             allocate = self.addAction({'verb': 'allocate','object': region})
 #                                      tree.desymbolize(world.symbols))
             risk = stateKey(region,'risk')
-            self.setReward(minimizeFeature(risk,self.name),
-                           likert[5][config.getint('System','reward_health')-1])
             tree = makeTree(approachMatrix(risk,likert[5][config.getint('System','system_impact')-1],
                                            0.))
             world.setDynamics(risk,allocate,tree,codePtr=True)
