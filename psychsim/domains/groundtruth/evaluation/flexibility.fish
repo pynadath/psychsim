237bb3634975716e25a4ac3b1d860efca8abfef0
David V. Pynadath <david@pynadath.net>
2018-10-18 01:19:44 -0700

    Modified instances for flexibility test

--- psychsim/domains/groundtruth/evaluation/flexibility.fish
+++ psychsim/domains/groundtruth/evaluation/flexibility.fish
@@ -1,14 +1,5 @@
 #!/usr/bin/fish
 cd (dirname (status -f))
-set -x PYTHONHASHSEED=1
-python3 . -n 3 -i 0 -r 10
-python3 . -n 3 -i 1 -r 10
-python3 . -n 3 -i 2 -r 10
-python3 . -n 3 -i 3 -r 10
-python3 . -n 3 -i 4 -r 10
-python3 . -n 3 -i 5 -r 10
-python3 . -n 3 -i 6 -r 10
-python3 . -n 3 -i 7 -r 10
-python3 . -n 3 -i 8 -r 10
-python3 . -n 3 -i 9 -r 10
+../gt.sh -i 30 -n 6 -r 10
+../gt.sh -i 31 -n 6 -r 10
 
