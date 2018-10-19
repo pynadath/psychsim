72ee8a0e924d34436fdf774edb7d0c3731a5063b
David V. Pynadath <david@pynadath.net>
2018-10-18 23:00:25 -0700

    Execute additional flexibility tests

--- psychsim/domains/groundtruth/evaluation/flexibility.fish
+++ psychsim/domains/groundtruth/evaluation/flexibility.fish
@@ -2,4 +2,10 @@
 cd (dirname (status -f))
 ../gt.sh -i 30 -n 6 -r 10
 ../gt.sh -i 31 -n 6 -r 10
+../gt.sh -i 32 -n 6 -r 10
+../gt.sh -i 33 -n 6 -r 10
+../gt.sh -i 34 -n 6 -r 10
+../gt.sh -i 35 -n 6 -r 10
+../gt.sh -i 36 -n 6 -r 10
+../gt.sh -i 37 -n 6 -r 10
 
