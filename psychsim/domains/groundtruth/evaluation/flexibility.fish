c21a6a584f8bbe88444f350be70d0d570045537c
David V. Pynadath <david@pynadath.net>
2018-10-20 18:00:55 -0700

    New flexibility instances

--- psychsim/domains/groundtruth/evaluation/flexibility.fish
+++ psychsim/domains/groundtruth/evaluation/flexibility.fish
@@ -1,11 +1,12 @@
 #!/usr/bin/fish
 cd (dirname (status -f))
-../gt.sh -i 30 -n 6 -r 10
-../gt.sh -i 31 -n 6 -r 10
-../gt.sh -i 32 -n 6 -r 10
-../gt.sh -i 33 -n 6 -r 10
-../gt.sh -i 34 -n 6 -r 10
-../gt.sh -i 35 -n 6 -r 10
-../gt.sh -i 36 -n 6 -r 10
-../gt.sh -i 37 -n 6 -r 10
+cd ..
+./gt.sh -i 30 -n 6 -r 10 --pickle -d INFO
+./gt.sh -i 31 -n 6 -r 10 --pickle -d INFO
+./gt.sh -i 32 -n 6 -r 10 --pickle -d INFO
+./gt.sh -i 33 -n 6 -r 10 --pickle -d INFO
+./gt.sh -i 34 -n 6 -r 10 --pickle -d INFO
+./gt.sh -i 35 -n 6 -r 10 --pickle -d INFO
+./gt.sh -i 36 -n 6 -r 10 --pickle -d INFO
+./gt.sh -i 37 -n 6 -r 10 --pickle -d INFO
 
