a79d5e9fd14af133972b846ad895d0d6f5970db7
David V. Pynadath <david@pynadath.net>
2018-11-07 00:50:30 +1100

    Stubs for next wave of TA2 research requests

---
+++ psychsim/domains/groundtruth/accessibility/Phase1/Explain/TA2A/TA2A-TA1C-0012/TA2A-TA1C-0012.py
@@ -0,0 +1,71 @@
+"""
+Research method category:
+Survey
+ 
+Specific question:
+We will send a team of trained survey professionals across all 16 regions, to homes, shelters and hospitals and ask the following questions:
+ 
+1. Unique ID corresponding to IDs in the ActorPostTable.tsv if possible.
+  
+2. Ask the following details if NOT able to link IDs (following question 1) or the respondent was NOT present in the original survey (as answered in the PostSurvey):
+a. Age?
+b. Children?
+c. Ethnicity?
+d. Religion?
+e. Fulltime Job?
+f. Gender?
+g. Pets?
+h. Residence?
+i. If went to shelter; wealth at shelter?
+j. How many hurricanes struck region this season?
+k. How severe was the most severe hurricane this season?
+ 
+3. Residency:
+a. Are you a short-term resident, long-term resident, or a visitor to the overall area?
+b. Are you a short-term resident, long-term resident, or a visitor to the specific region you in which you are currently residing?
+
+4. Additional personal information:
+a. Highest grade of schooling completed
+b. Occupation
+c. What is the size of your family and close interpersonal network — people whom you could call upon for significant support — within your region?
+d. What are the number of your acquaintances within your region?
+ 
+5. Prior season experience:
+a. Have you experienced one or more hurricanes in prior seasons?
+b. Were deaths involved in any previously experienced hurricanes in your region?
+c. Were deaths involved in any previously experienced hurricanes among your family members or close interpersonal ties?
+d. Were deaths involved in any previously experienced hurricanes among your acquaintances?
+e. Were casualties involved in any previously experienced hurricanes in your region?
+f. Were casualties involved in any previously experienced hurricanes among your family members or close interpersonal ties?
+g. Were casualties involved in any previously experienced hurricanes among your acquaintances?
+h. Did any experienced hurricanes from previous seasons pose a significant risk to myself and my family? (Response on a 1-5 scale, ranging from “strongly disagree” to “strongly agree”)
+i. How many experienced hurricanes posed a significant risk to myself and my family?
+j. Has any other natural disaster posed a significant risk to myself or my family? (Response on a 1-5 scale, ranging from “strongly disagree” to “strongly agree”)
+k. Has any other circumstance posed a significant risk to myself or my family? (Response on a 1-5 scale, ranging from “strongly disagree” to “strongly agree”)
+l. In your experience, has the government previously tended to over-or under-evacuate the regional population from hurricanes in previous seasons? (Response on a 1-5 scale, ranging from “under-evacuate” to “over-evacuate”)
+
+6. Current experience:
+a. Were casualties involved in any experienced hurricanes this season among your family members or close interpersonal ties?
+b. Were casualties involved in any experienced hurricanes this season among your acquaintances?
+ 
+7. Attitudes:
+a. Should the government should intervene on behalf of my health and safety? (Response on a 1-5 scale, ranging from “I strongly do believe that the government should not intervene” to “I strongly believe that the government should intervene”)
+b. When would I evacuate a hurricane? (Multiple choice / multiple answer: answer “Yes” as many as are applicable: “If there is a potential threat of some property damage”; “If there is a strong threat of some property damage”; “If there is a potential threat of some widespread property damage”; “If there is a strong threat of widespread property damage”; “If there is a potential threat of some injury (“casualty”)”; “If there is a strong threat of some injury(“casualty”)”; “If there is a potential threat of widespread injury (“casualty”)”; “If there is a strong threat of widespread injury(“casualty”)”;“If there is a potential threat of some death”; “If there is a strong threat of some death”; “If there is a potential threat of widespread death”; “If there is a strong threat of widespread death”)  
+ 
+7. Description:
+a. How does a hurricane pose a significant risk to myself and my family? [short answer, e.g., damage house and indirectly kill or injure; directly kill or injure; cut off from water and food that can inconvenience and passively injure; etc.]
+b. If the government’s effort to evacuate the regional population were unsatisfying, why? [multiple choice: “They should have evacuated, but did not”; “they did not evaluate, but should have”; “their information about the hurricane was insufficient for them to make an effective evacuation decision”.]
+ 
+8. Media:
+a. Who reports deaths and casualties? (e.g., government, private news service).
+b. Do you trust these reports?
+ 
+Sampling strategy:
+We seek this information on a paired sample with existing pre- and post-surveys. If these links are unavailable, then we seek a random sample of the population, of maximal size that we can afford.
+ 
+Other applicable detail:
+Not applicable
+ 
+Research request identifier:
+TA2A-TA1C-0012-RR
+"""
