cc7d162af38d1de1bf2e595c57f06f0b2ab736b1
David V. Pynadath <david@pynadath.net>
2018-11-21 00:11:24 -0800

    Reusable methods for reading hurricanes and social network

--- psychsim/domains/groundtruth/simulation/data.py
+++ psychsim/domains/groundtruth/simulation/data.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import csv
+import os.path
 import random
 
 likert = {5: [0.2,0.4,0.6,0.8,1.],
@@ -35,4 +37,59 @@ mapFromTandE = {'Actor’s distribution over ethnicGroup\'': ('Actors','ethnic_m
                 'Region’s risk distribution\'': ('Regions','risk_value',None),
                 'Region’s security distribution\'': ('Regions','security_value',None),
                 }
-    
+
+def readHurricanes(instance,run=0):
+    """
+    :returns: A list of attributes about the hurricanes so far
+    """
+    inFile = os.path.join(os.path.dirname(__file__),'..','Instances','Instance%d' % (instance),
+                          'Runs','run-%d' % (run),'HurricaneTable.tsv')
+    hurricanes = []
+    with open(inFile,'r') as csvfile:
+        reader = csv.DictReader(csvfile,delimiter='\t')
+        for row in reader:
+            hurricane = int(row['Name'])
+            if len(hurricanes) < hurricane:
+                hurricanes.append({'Hurricane': hurricane,
+                                   'Predicted Location': row['Location'],
+                                   'Media Coverage': 'yes',
+                                   'Actual Track': [],
+                                   'Official Announcements': 'none',
+                                   'Start': int(row['Timestep']),
+                                   })
+            elif row['Landed'] == 'yes':
+                if row['Location'] == 'leaving':
+                    hurricanes[-1]['End'] = int(row['Timestep'])
+                else:
+                    if not 'Actual Severity' in hurricanes[-1]:
+                        hurricanes[-1]['Actual Severity'] = row['Category']
+                    if len(hurricanes[-1]['Actual Track']) == 0 or \
+                       hurricanes[-1]['Actual Track'][-1] != row['Location']:
+                        hurricanes[-1]['Actual Track'].append(row['Location'])
+    for record in hurricanes:
+        record['Actual Track'] = ','.join(record['Actual Track'])
+    if 'End' not in hurricanes[-1]:
+        # Ignore partial hurricane track
+        return hurricanes[:-1]
+    else:
+        return hurricanes
+
+def readNetwork(instance,run=0):
+    """
+    :returns: The social networks in the given run
+    """
+    networks = {}
+    inFile = os.path.join(os.path.dirname(__file__),'..','Instances','Instance%d' % (instance),
+                          'Runs','run-%d' % (run),'RelationshipDataTable.tsv')
+    with open(inFile,'r') as csvfile:
+        reader = csv.DictReader(csvfile,delimiter='\t')
+        for row in reader:
+            link = row['RelationshipType']
+            if link not in networks:
+                networks[link] = {}
+            assert row['Directed'] == 'yes'
+            try:
+                networks[link][row['FromEntityId']].add(row['ToEntityId'])
+            except KeyError:
+                networks[link][row['FromEntityId']] = {row['ToEntityId']}
+    return networks
