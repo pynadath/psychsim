920c38cecc539eaee30680428f71a79584793a94
David V. Pynadath <david@pynadath.net>
2018-11-26 01:37:09 -0800

    Instances for flexibility-complexity evaluation

---
+++ psychsim/domains/groundtruth/evaluation/complexity.fish
@@ -0,0 +1,14 @@
+#!/usr/bin/fish
+cd (dirname (status -f))
+cd ..
+python3 simulate.py -i 70 -n 6 -r 10 --pickle -d INFO
+python3 simulate.py -i 71 -n 6 -r 10 --pickle -d INFO
+python3 simulate.py -i 72 -n 6 -r 10 --pickle -d INFO
+python3 simulate.py -i 73 -n 6 -r 10 --pickle -d INFO
+python3 simulate.py -i 74 -n 6 -r 10 --pickle -d INFO
+python3 simulate.py -i 75 -n 6 -r 10 --pickle -d INFO
+python3 simulate.py -i 76 -n 6 -r 10 --pickle -d INFO
+python3 simulate.py -i 77 -n 6 -r 10 --pickle -d INFO
+python3 simulate.py -i 78 -n 6 -r 10 --pickle -d INFO
+python3 simulate.py -i 79 -n 6 -r 10 --pickle -d INFO
+python3 simulate.py -i 80 -n 6 -r 10 --pickle -d INFO
