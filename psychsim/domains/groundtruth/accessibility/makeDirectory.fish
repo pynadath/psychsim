c8c40ed7e644e18a59168742c55d091b2d70d30f
David V. Pynadath <david@pynadath.net>
2019-06-07 00:35:08 -0700

    Optional argument on which challenge

--- psychsim/domains/groundtruth/accessibility/makeDirectory.fish
+++ psychsim/domains/groundtruth/accessibility/makeDirectory.fish
@@ -1,4 +1,10 @@
-for i in (seq 1 14)
+set lo 1
+if test (count $argv) -gt 0
+	if test $argv[1] = "prescribe"
+		set lo 9
+	end
+end
+for i in (seq $lo 14)
 	mkdir -p Instances/Instance$i/Runs/run-0
 end
 mkdir -p SimulationDefinition
