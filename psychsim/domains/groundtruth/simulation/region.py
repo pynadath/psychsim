2908a1ded68cebd41c46053878f188a1287df443
David V. Pynadath <david@pynadath.net>
2019-12-14 02:56:50 -0800

    Compute relevant regions when traveling to shelter or evacuating

--- psychsim/domains/groundtruth/simulation/region.py
+++ psychsim/domains/groundtruth/simulation/region.py
@@ -9,7 +9,7 @@ else:
 
 from psychsim.agent import Agent
 
-from psychsim.domains.groundtruth.simulation.data import likert,toLikert,sampleNormal
+from psychsim.domains.groundtruth.simulation.data import likert,toLikert,sampleNormal,logNode
 
 class Region(Agent):
     nameString = 'Region%02d'
@@ -47,7 +47,8 @@ class Region(Agent):
 
         self.setAttribute('static',True)
         
-        #//GT: node 34; 1 of 1; next 15 lines
+        logNode('Region\'s risk','Risk level of region','Real in [0-1]')
+        #//GT: node 1; 1 of 1; next 15 lines
         risk = world.defineState(self.name,'risk',float,description='Level of risk from hurricane',
                                  codePtr=True)
         try:
@@ -65,6 +66,8 @@ class Region(Agent):
         world.setFeature(risk,self.risk)
 
         if config.getint('Simulation','phase',fallback=1) == 1:
+            logNode('Region\'s security','Security level of Region','Real in [0-1]')
+            #
             security = world.defineState(self.name,'security',float,codePtr=True,
                                          description='Level of law enforcement in region')
             try:
@@ -77,11 +80,13 @@ class Region(Agent):
                 else:
                     self.security = likert[5][mean-1]
             world.setFeature(security,self.security)
+            
         mean = config.getint('Regions','economy_mean',fallback=0)
         if mean == 0:
             self.economy = None
         else:
-            #//GT: node 46; 1 of 1; next 8 lines
+            logNode('Region\'s economy','Economic strength of Region','Real in [0-1]')
+            #//GT: node 2; 1 of 1; next 8 lines
             sigma = config.getint('Regions','economy_sigma')
             if sigma > 0:
                 self.economy = sampleNormal(mean,sigma)
@@ -91,29 +96,37 @@ class Region(Agent):
                                         description='Current economic level of region')
             world.setFeature(economy,self.economy)
 
-        #//GT: node 35; 1 of 1; next 8 lines
         if index is not None:
             # Shelter in this region
+            logNode('Region\'s shelterRisk','Risk level of shelter','Real in [0-1]')
+            #//GT: node 3; 1 of 1; next 6 lines
             world.defineState(self.name,'shelterRisk',float,codePtr=True)
             riskLevel = int(config.get('Shelter','risk').split(',')[index])
             if riskLevel > 0:
                 self.setState('shelterRisk',likert[5][riskLevel-1])
             else:
                 self.setState('shelterRisk',0.)
+
+            logNode('Region\'s shelterPets','Whether shelter allows pets','Boolean')
+            #//GT: node 4; 1 of 1; next 5 lines
             world.defineState(self.name,'shelterPets',bool,codePtr=True)
-        #//GT: node 36; 1 of 1; next 4 lines
             if config.get('Shelter','pets').split(',')[index] == 'yes':
                 self.setState('shelterPets',True)
             else:
                 self.setState('shelterPets',False)
+
             self.capacity = int(config.get('Shelter','capacity').split(',')[index])
             if self.capacity > 0:
-                #//GT: node 44; 1 of 1; next 2 lines
+                logNode('Region\'s shelterCapacity','Maximum number of people that the shelter can accommodate','Positive integer')
+                #//GT: node 5; 1 of 1; next 2 lines
                 world.defineState(self.name,'shelterCapacity',int,codePtr=True)
                 self.setState('shelterCapacity',self.capacity)
-                #//GT: node 45; 1 of 1; next 2 lines
+
+                logNode('Region\'s shelterOccupancy','Current number of people in this shelter','Nonnegative integer')
+                #//GT: node 6; 1 of 1; next 2 lines
                 world.defineState(self.name,'shelterOccupancy',int,codePtr=True)
                 self.setState('shelterOccupancy',0)
+
         else:
             self.capacity = 0
 
@@ -123,6 +136,38 @@ class Region(Agent):
         else:
             return abs(region.x-self.x) + abs(region.y-self.y)
     
+    def path(self,region):
+        path = [self.name]
+        while self.world.agents[path[-1]].x > self.world.agents[region].x:
+            path.append(self.world.agents[path[-1]].west)
+        while self.world.agents[path[-1]].x < self.world.agents[region].x:
+            path.append(self.world.agents[path[-1]].east)
+        while self.world.agents[path[-1]].y > self.world.agents[region].y:
+            path.append(self.world.agents[path[-1]].north)
+        while self.world.agents[path[-1]].y < self.world.agents[region].y:
+            path.append(self.world.agents[path[-1]].south)
+        return path
+
+    def evacuationPath(self,direction=None):
+        """
+        :warning: Assumes coastline is on the west (i.e., can't evacuate that way)
+        """
+        if self.east == 'none' or self.north == 'none' or self.south == 'none':
+            return [self.name]
+        else:
+            if direction is None or direction == 'east':
+                path = [self.name]+self.world.agents[self.east].evacuationPath('east')
+            else:
+                path = None
+            if direction is None or direction == 'north':
+                newPath = [self.name]+self.world.agents[self.north].evacuationPath('north')
+                if path is None or len(newPath) < len(path):
+                    path = newPath
+            if direction is None or direction == 'south':
+                newPath = [self.name]+self.world.agents[self.south].evacuationPath('south')
+                if path is None or len(newPath) < len(path):
+                    path = newPath
+            return path
 
     def setInhabitants(self,agents):
         self.inhabitants = agents
