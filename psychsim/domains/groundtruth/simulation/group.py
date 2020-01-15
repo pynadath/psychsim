b3d42bbe2afde0cb493001505449c27fc07fd232
David V. Pynadath <david@pynadath.net>
2020-01-15 01:49:34 -0800

    Fixed some projection bugs

--- psychsim/domains/groundtruth/simulation/group.py
+++ psychsim/domains/groundtruth/simulation/group.py
@@ -25,9 +25,10 @@ class Group(Agent):
         
         if config.getboolean('Groups','prorisk'):
             logNode('Group-decreaseRisk','Action choice of performing a prosocial action as a group','Boolean')
-            #//GT: node 48; 1 of 1; next 5 lines
+            #//GT: node 46; 1 of 1; next 6 lines
+            tree = {'if': equalRow(stateKey('Nature','phase'),'none'),True: False,False: True}
             if name in regions:
-                actGoodRisk = self.addAction({'verb': 'decreaseRisk','object': name},codePtr=True)
+                actGoodRisk = self.addAction({'verb': 'decreaseRisk','object': name},makeTree(tree).desymbolize(world.symbols),codePtr=True)
             else:
                 actGoodRisk = self.addAction({'verb': 'decreaseRisk'},codePtr=True)
                 name ='Region01'
@@ -72,18 +73,18 @@ class Group(Agent):
 #            goHome = self.addAction({'verb': 'returnHome'},codePtr=True)
 
         logNode('Group-noDecision','Group chose not to perform any joint action','Boolean')
-        #//GT: node 49; 1 of 1; next 1 lines
+        #//GT: node 47; 1 of 1; next 1 lines
         self.nop = self.addAction({'verb': 'noDecision'},codePtr=True)
 
         logNode('Group\'s horizon','Number of steps into future that group uses to evaluate candidate action choices','Positive integer')
-        #//GT: node 50; 1 of 1; next 1 lines
+        #//GT: node 48; 1 of 1; next 1 lines
         self.setAttribute('horizon',config.getint('Groups','horizon'))
 
         self.potentials = None
         # Belief aggregation
         logNode('Group\'s beliefAggregation','Degree to which group\'s aggregate beliefs are biased toward those of its more optimistic/pessimistic members',
-            'String: "mean" / "max" / "min"')
-        #//GT: node 51; 1 of 1; next 16 lines
+            'String: "mean" / "max" / "min"','Static')
+        #//GT: node 49; 1 of 1; next 16 lines
         aggregators = ['mean']
         weights = [5]
         value = config.getint('Groups','belief_max')
@@ -109,7 +110,7 @@ class Group(Agent):
         count = 0
         for name in agents:
             logNode('Actor memberOf Group','Group membership relationship','Boolean')
-            #//GT: node 52; 1 of 1; next 10 lines
+            #//GT: node 50; 1 of 1; next 10 lines
             agent = self.world.agents[name]
             member = self.world.defineRelation(name,self.name,'memberOf',bool,codePtr=True)
             # Join a group
@@ -122,7 +123,7 @@ class Group(Agent):
                     count += 1
 
             logNode('Actor-join-Group','Action choice of joining a group','Boolean')
-            #//GT: node 53; 1 of 1; next 5 lines
+            #//GT: node 51; 1 of 1; next 5 lines
             tree = {'if': trueRow(member),True: False, False: True}
             if self.config.getint('Simulation','phase',fallback=1) < 3:
                 tree = {'if': trueRow(stateKey(name,'alive')),True: tree,False: False}
@@ -130,7 +131,7 @@ class Group(Agent):
                                    makeTree(tree).desymbolize(self.world.symbols),codePtr=True)
 
             logEdge('Actor-join-Group','Actor memberOf Group','sometimes','Joining a group makes one a member of it')
-            #//GT: edge 61; from 53; to 52; 1 of 1; next 2 lines
+            #//GT: edge 63; from 51; to 50; 1 of 1; next 2 lines
             tree = makeTree(setTrueMatrix(member))
             self.world.setDynamics(member,join,tree,codePtr=True)
 
@@ -139,7 +140,7 @@ class Group(Agent):
 
             # Leave a group
             logNode('Actor-leave-Group','Action choice of leaving a group','Boolean')
-            #//GT: node 54; 1 of 1; next 5 lines
+            #//GT: node 52; 1 of 1; next 5 lines
             tree = {'if': trueRow(member),True: True, False: False}
             if self.config.getint('Simulation','phase',fallback=1) < 3:
                 tree = {'if': trueRow(stateKey(name,'alive')),True: tree,False: False}
@@ -147,7 +148,7 @@ class Group(Agent):
                                     makeTree(tree).desymbolize(self.world.symbols),codePtr=True)
 
             logEdge('Actor-leave-Group','Actor memberOf Group','sometimes','Leaving a group makes one no longer a member of it')
-            #//GT: edge 62; from 54; to 52; 1 of 1; next 2 lines
+            #//GT: edge 64; from 52; to 50; 1 of 1; next 2 lines
             tree = makeTree(setFalseMatrix(member))
             self.world.setDynamics(member,leave,tree,codePtr=True)
 
@@ -159,20 +160,16 @@ class Group(Agent):
                 #
                 logEdge('Actor memberOf Group','Actor\'s Expected Reward','sometimes','Attachment style can make person happier/sadder to be in a group')
                 #
-                attachment = stateKey(name,'attachment')
-                R = rewardKey(name)
-                tree = makeTree({'if': thresholdRow(stateKey(name,'risk'),
+                if agent.attachment == 'anxious' or agent.attachment == 'avoidant':
+                    R = rewardKey(name)
+                    tree = makeTree({'if': thresholdRow(stateKey(name,'risk'),
                                                     likert[5][self.config.getint('Actors','attachment_threshold')]),
-                                 True: {'if': equalRow(attachment,'anxious'),
-                                        True: setToFeatureMatrix(R,member,1.),
-                                        False: {'if': equalRow(attachment,'avoidant'),
-                                                True: setToFeatureMatrix(R,member,-1.),
-                                                False: setToConstantMatrix(R,0.)}},
+                                 True: setToFeatureMatrix(R,member,1. if agent.attachment == 'anxious' else -1.),
                                  False: setToConstantMatrix(R,0.)})
-                agent.setReward(tree,likert[5][self.config.getint('Actors','attachment_r')-1])
+                    agent.setReward(tree,likert[5][self.config.getint('Actors','attachment_r')-1])
 
-            logNode('Group\'s magnification','Magnification of effect of joint action by Group (as a function of size of the group)','Real in [0-1]')
-            #//GT: node 55; 1 of 1; next 1 lines
+            logNode('Group\'s magnification','Magnification of effect of joint action by Group (as a function of size of the group)','Real in [0-1]','Static')
+            #//GT: node 53; 1 of 1; next 1 lines
             magnify = self.config.getint('Groups','magnification')
 
             if self.config.getboolean('Groups','prorisk') and magnify > 0:
@@ -183,12 +180,12 @@ class Group(Agent):
                                (action.get('object') is None or \
                                 lonely['object'] == action['object']):
                                 # Magnify effect of prosocial behavior
-                                logEdge('Group-decreaseRisk-Region','Region\'s risk','sometimes','Performing prosocial behavior reduces the risk in the target region')
-                                #//GT: edge 63; from 56; to 1; 1 of 1; next 15 lines
+                                logEdge('Group-decreaseRisk','Region\'s risk','sometimes','Performing prosocial behavior reduces the risk in the target region')
+                                #//GT: edge 65; from 46; to 1; 1 of 1; next 15 lines
                                 logEdge('Actor memberOf Group','Region\'s risk','sometimes','Performing prosocial behavior as a group reduces the risk in the target region more')
-                                #//GT: edge 64; from 52; to 1; 1 of 1; next 13 lines
+                                #//GT: edge 66; from 50; to 1; 1 of 1; next 13 lines
                                 logEdge('Group\'s magnification','Region\'s risk','sometimes','Performing prosocial behavior as a group reduces the risk in the target region more')
-                                #//GT: edge 65; from 55; to 1; 1 of 1; next 11 lines
+                                #//GT: edge 67; from 53; to 1; 1 of 1; next 11 lines
                                 risk = stateKey(lonely['object'],'risk')
                                 tree = self.world.dynamics[risk][lonely]
                                 assert tree.isLeaf(),'Unable to magnify branching trees'
@@ -239,9 +236,15 @@ class Group(Agent):
         if state is None:
             state = self.world.state
         return [agent for agent in self.potentials \
-                if self.world.getFeature(binaryKey(agent,self.name,'memberOf')).first()]
+                if binaryKey(agent,self.name,'memberOf') in state and self.world.getFeature(binaryKey(agent,self.name,'memberOf'),state,True)]
 
     def reward(self,state=None,model=None,recurse=False):
+        logNode('Group\'s Expected Reward','Expected utility (desirability) of candidate actions','Real for each action choice')
+        #//GT: node 56; 1 of 1; next 26 lines
+        logEdge('Actor\'s Expected Reward','Group\'s Expected Reward','often','Expected rewards of joint actions derived from individual members\' expected rewards')
+        #//GT: edge 73; from 35; to 56; 1 of 1; next 24 lines
+        logEdge('Actor memberOf Group','Group\'s Expected Reward','often','Expected rewards of joint actions derived from individual members\' expected rewards')
+        #//GT: edge 74; from 50; to 56; 1 of 1; next 22 lines
         if state is None:
             state = self.world.state
         if model is None:
@@ -249,19 +252,31 @@ class Group(Agent):
         total = 0.
         for name in self.members(state):
             agent = self.world.agents[name]
-            ER = agent.reward(state)  
+            R = agent.getReward()
+            assert len(R) == 1
+            tree = next(iter(R.values()))
+            if tree.isLeaf():
+                vector = tree.getLeaf()[makeFuture(rewardKey(name))]
+            else:
+                # Should be only because of attachment adding a reward for group membership, which is a moot point now
+                vectors = [subtree[makeFuture(rewardKey(name))] for subtree in tree.leaves()]
+                vector = min(vectors,key=lambda l: len(l))
+            ER = 0.
+            for key,weight in vector.items():
+                if key != CONSTANT:
+                    ER += weight*self.world.getFeature(key,state).expectation()
             total += ER
         return total
     
     def getBelief(self,state=None,model=None):
         logNode('GroupBeliefOfRegion\'s risk','Belief about risk level of home region','Probability Distribution over reals in [0-1]')
-        #//GT: node 58; 1 of 1; next 85 lines
+        #//GT: node 55; 1 of 1; next 46 lines
         logEdge('Actor memberOf Group','GroupBeliefOfRegion\'s risk','often','Group\'s belief is a function of its members\' beliefs')
-        #//GT: edge 66; from 52; to 58; 1 of 1; next 83 lines
+        #//GT: edge 70; from 50; to 55; 1 of 1; next 44 lines
         logEdge('ActorBeliefOfRegion\'s risk','GroupBeliefOfRegion\'s risk','often','Group\'s belief is a function of its members\' beliefs')
-        #//GT: edge 67; from 12; to 58; 1 of 1; next 81 lines
+        #//GT: edge 71; from 12; to 55; 1 of 1; next 42 lines
         logEdge('Group\'s beliefAggregation','GroupBeliefOfRegion\'s risk','often','Group\'s belief is a function of its members\' beliefs')
-        #//GT: edge 68; from 51; to 58; 1 of 1; next 79 lines
+        #//GT: edge 72; from 49; to 55; 1 of 1; next 40 lines
         if state is None:
             state = self.world.state
         if model is None:
@@ -274,45 +289,46 @@ class Group(Agent):
                 models[name] = self.world.getModel(name,state)
             else:
                 models[name] = self.world.getModel(name,self.world.state)
-            assert len(models[name]) == 1
-            models[name] = models[name].first()
+            if isinstance(models[name],Distribution):
+                assert len(models[name]) == 1
+                models[name] = models[name].first()
         beliefs = {name: self.world.agents[name].getBelief(state,models[name])
                    for name in members}
         belief = VectorDistributionSet()
         for key in [turnKey(name) for name in [self.name,'Nature','System']]+[turnKey(name) for name in members]+\
             [actionKey(name) for name in [self.name,'Nature','System']]+[actionKey(name) for name in members]+\
             [binaryKey(name,self.name,'memberOf') for name in members]+[modelKey(name) for name in members]:
-            self.world.setFeature(key,self.world.getFeature(key,state),belief)
+            if key in state:
+                self.world.setFeature(key,self.world.getFeature(key,state),belief)
         uncertain = {stateKey('Nature','category'): None,
             stateKey(self.name[5:],'risk'): None}
+        if self.config.getboolean('Groups','evacuate',fallback=False) and self.config.getboolean('Disaster','evacuation_path_risk',fallback=False):
+            path = self.world.agents[self.name[5:]].evacuationPath()
+            uncertain[stateKey(path[-1],'risk')] = None
         for subbelief in beliefs.values():
             for key in subbelief.keys():
                 if isStateKey(key) and state2feature(key) == 'shelterRisk':
                     uncertain[key] = None
+        for feature in ['phase','days','location']:
+            # Use real values, because any disagreement are ignorable
+            subbelief = self.world.getState('Nature',feature,state)
+            self.world.setState('Nature',feature,subbelief,belief)
         for name in members:
             for key in {stateKey(name,'risk'),stateKey(name,'location'),stateKey(name,'employed')}|\
                 self.world.agents[name].getReward(models[name]).getKeysIn()-{CONSTANT}:
                 self.world.setFeature(key,self.world.getFeature(key,beliefs[name]),belief)
-            for feature in ['phase','days','location']:
-                subbelief = self.world.getState('Nature',feature,beliefs[name])
-                if stateKey('Nature',feature) in belief:
-                    # Everyone should agree on this feature
-                    if self.world.getState('Nature',feature,belief) != subbelief:
-                        print(feature)
-                        print(subbelief)
-                        print(self.world.getState('Nature',feature,belief))
-                else:
-                    self.world.setState('Nature',feature,subbelief,belief)
 
             for key in uncertain:
                 subbelief = self.world.getFeature(key,beliefs[name])
                 if uncertain[key] is None:
                     uncertain[key] = {value: subbelief[value] for value in subbelief.domain()}
+                    if self.aggregator != 'mean':
+                        uncertain[key] = Distribution(uncertain[key])
                 elif self.aggregator == 'max':
-                    if subbelief.expectation() > belief.expectation():
+                    if subbelief.expectation() > uncertain[key].expectation():
                         uncertain[key] = subbelief
                 elif self.aggregator == 'min':
-                    if subbelief.expectation() < belief.expectation():
+                    if subbelief.expectation() < uncertain[key].expectation():
                         uncertain[key] = subbelief
                 else:
                     assert self.aggregator == 'mean'
@@ -337,13 +353,20 @@ class Group(Agent):
                     return{'action': action}
         if model is None:
             model = self.world.getModel(self.name,state)
-            assert len(model) == 1,'Currently unable to decide under uncertain models'
-            model = model.first()
+            if isinstance(model,Distribution):
+                assert len(model) == 1,'Currently unable to decide under uncertain models'
+                model = model.first()
         horizon = self.getAttribute('horizon',model)
         V = {}
         risk = {}
         risks = {}
         health = {}
+
+        for action in actions:
+            logEdge('GroupBeliefOfRegion\'s risk','Group-%s' % (action['verb']),'often','Group decisions are influenced by their beliefs of the risk')
+            #//GT: edge 92; from 55; to 46; 1 of 1; next 33 lines
+            logEdge('Group\'s horizon','Group-%s' % (action['verb']),'often','Group decisions are influenced by how far into the future they consider')
+            #//GT: edge 93; from 48; to 46; 1 of 1; next 31 lines
         belief = self.getBelief(state,model)
         order = [{k for k in keySet if k in belief} for keySet in self.world.dependency.getEvaluation()]
         order = [keySet for keySet in order if keySet]
@@ -368,12 +391,14 @@ class Group(Agent):
                         joint[name] = actor.nop
             current = copy.deepcopy(belief)
             for t in range(horizon):
-                self.world.step({self.name: action},current,keySubset=belief.keys(),updateBeliefs=False)
+                self.world.step({self.name: action},current,keySubset=belief.keys(),select='max',updateBeliefs=False)
                 self.world.step(joint,current,keySubset=belief.keys(),updateBeliefs=False)
                 self.world.step({'System': self.world.agents['System'].nop},current,keySubset=belief.keys(),updateBeliefs=False)
-                self.world.step({'Nature': evolve},current,keySubset=belief.keys(),updateBeliefs=False)
+                self.world.agents['Nature'].step('max',current,False)
+#                self.world.step({'Nature': evolve},current,keySubset=belief.keys(),updateBeliefs=False)
             assert model is not None
             V[action] = self.reward(current,model)
+
         best = None
         for action,value in V.items():
             if best is None or value > best[1]:
