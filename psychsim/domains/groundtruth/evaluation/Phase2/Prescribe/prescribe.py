e9615355b851ac9288becd1388b2c4535827240f
David V. Pynadath <david@pynadath.net>
2020-02-19 01:29:57 -0800

    Break short-term challenge down in to separate constrained/unconstrained evaluations

--- psychsim/domains/groundtruth/evaluation/Phase2/Prescribe/prescribe.py
+++ psychsim/domains/groundtruth/evaluation/Phase2/Prescribe/prescribe.py
@@ -5,32 +5,42 @@ from psychsim.domains.groundtruth import accessibility
 
 targets = {20: 'Actor0461', 21: 'Actor0361'}
 if __name__ == '__main__':
-    fields = ['Challenge','Baseline Casualties','TA2A Casualties','TA2B Casualties','Baseline Injury','TA2A Injury','TA2B Injury']
+    fields = ['Challenge']
     output = []
     for instance,args in accessibility.instanceArgs('Phase2','Prescribe'):
         print('Instance %d (%d,%d)' % (instance,args['instance'],args['run']))
+        runs = ['Actual','TA2A','TA2B']
+        if instance == 20:
+            entry = {'Challenge': 'Short-term'}
+            runs += ['TA2AConstrained','TA2AUnconstrained','TA2BConstrained','TA2BUnconstrained']
+        else:
+            entry = {'Challenge': 'Long-term'}
         config = accessibility.getConfig(args['instance'])
         states = {}
         # Load world
         world = accessibility.unpickle(instance)
         actors = [name for name in world.agents if name[:5] == 'Actor' and world.getState(name,'alive',unique=True)]
         rootDir = accessibility.getDirectory(args)
-        if instance == 20:
-            entry = {'Challenge': 'Short-term'}
-        else:
-            entry = {'Challenge': 'Long-term'}
-        for sub in ['Actual','TA2A','TA2B']:
+        for sub in runs:
             path = os.path.join(rootDir,sub)
             times = [int(name[5:-10]) for name in os.listdir(path) if name[-10:] == 'Nature.pkl']
             states.clear()
             accessibility.loadState(args,states,max(times),'Nature',world,sub)
             casualties = len([name for name in actors if not world.getState(name,'alive',unique=True)])
             if sub == 'Actual':
-                entry['Baseline Casualties'] = casualties
-                entry['Baseline Injury'] = 0.
+                var = 'Baseline Casualties'
             else:
-                entry['%s Casualties' % (sub)] = casualties
-                entry['%s Injury' % (sub)] = 0.
+                var = '%s Casualties' % (sub)
+            entry[var] = casualties
+            if instance == 20:
+                fields.append(var)
+            if sub == 'Actual':
+                var = 'Baseline Injury'
+            else:
+                var = '%s Injury' % (sub)
+            entry[var] = 0.
+            if instance == 20:
+                fields.append(var)
         output.append(entry)
     with open('PrescribeResults.tsv','w') as csvfile:
         writer = csv.DictWriter(csvfile,fields,delimiter='\t',extrasaction='ignore')
